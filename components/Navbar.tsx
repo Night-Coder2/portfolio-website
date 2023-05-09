d132d79bf7786e1245be7233601b96fd942032a7
phoenixLegen <sudhircks@gmail.com>
2023-05-09 12:06:00 +0530

    sssssssssssssssssssssssssssssssssssssssssssssssssss

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -125,7 +125,7 @@ const Navbar = () => {
             </div>
             <div className='border-b border-gray-950 my-4'>
               <p className='w-[85%] md:w-[90%] py-4'>
-                Let&#39;s build something legendary together
+                Let&#39;s build something legendary together.
               </p>
             </div>
           </div>
