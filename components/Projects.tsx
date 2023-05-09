a1f755494e96f6d029550d0b9b803c5cf9d043d7
phoenixLegen <sudhircks@gmail.com>
2023-05-09 20:17:01 +0530

    projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import ProjectItem from './ProjectItems';
+import DiscordImg from '../public/discord-server-and-waterbot.png';
 
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
-}
+      <div id='projects' className='w-full'>
+        <div className='max-w-[1240px] mx-auto px-2 py-16'>
+          <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+            Projects
+          </p>
+          <h2 className='py-4'>What I&apos;ve Built</h2>
+          <div className='grid md:grid-cols-2 gap-8'>
+            <ProjectItem
+                title='Discord Server and WaterBot'
+                backgroundImg={DiscordImg}
+                projectUrl='/property'
+                tech='React JS'
+            />
+          </div>
+        </div>
+      </div>
+  );
+};
