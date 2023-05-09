d2b27f821511aac6e37caa4fcce6e1c6a1294e6f
phoenixLegen <sudhircks@gmail.com>
2023-05-09 13:16:44 +0530

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import About from "@/components/About";
 import Main from "@/components/Main";
 import Skills from "@/components/Skills";
-import Projects from "@/components/projects";
+import Projects from "@/components/Projects";
 
 export default function Page() {
   return (
