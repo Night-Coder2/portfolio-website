e417578c3d5dda8dd2c2c5d8432916e92e4a8f78
phoenixLegen <sudhircks@gmail.com>
2023-05-10 18:41:59 +0530

    skills and head

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import '@/styles/globals.css'
 import Head from 'next/head'
 import Navbar from '@/components/Navbar'
+import navLogo from '@/public/assets/navLogo.png'
 
 export default function RootLayout({
   children,
@@ -10,15 +11,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Sudhir&apos;s Portfolio</title>
-      </Head>
+      <head>
+
+          <title>Sudhir&apos;s Portfolio</title>
+          <meta charSet="UTF-8"/>
+          <meta name="description" content="My Portfolio"/>
+          <meta name="keywords" content="HTML, CSS, JavaScript"/>
+          <meta name="author" content="Sudhir Madhyastha"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </head>
       <body>
         <Navbar />
         {children}
+
       </body>
     </html>
   )
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,6 +7,7 @@ import typescript from '@/public/assets/Skills/typescript.png'
 import github from '@/public/assets/Skills/github.png'
 import javascript from '@/public/assets/Skills/javascript.png'
 import Image from 'next/image'
+import Link from "next/link";
 
 export default function Skills() {
   return (
@@ -16,74 +17,81 @@ export default function Skills() {
             <h2 className='py-4'>What I Can Do</h2>
             <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
                 <div className='p-6 shadow-xl rounded-xl bg-white hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <Link href={'https://www.w3schools.com/html/'}><div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src={html} alt="/" width={64} height={64} />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3 className='text-slate-950'>HTML</h3>
                         </div>
-                    </div>
+                    </div></Link>
+
                 </div>
                 <div className='p-6 shadow-xl rounded-xl bg-white hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <Link href={'https://react.dev'}><div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src={react} alt="/" width={64} height={64} />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3 className='text-slate-950'>React</h3>
                         </div>
-                    </div>
+                    </div></Link>
+
                 </div>
                 <div className='p-6 shadow-xl rounded-xl bg-white hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <Link href={'https://tailwindcss.com'}><div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src={tailwind} alt="/" width={64} height={64} />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3 className='text-slate-950'>Tailwind CSS</h3>
                         </div>
-                    </div>
+                    </div></Link>
+
                 </div>
                 <div className='p-6 shadow-xl rounded-xl bg-white hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                        <div className='m-auto'>
-                            <Image src={typescript} alt="/" width={64} height={64} />
+                    <Link href={'https://www.typescriptlang.org'}>
+                        <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                            <div className='m-auto'>
+                                <Image src={typescript} alt="/" width={64} height={64} />
+                            </div>
+                            <div className='flex flex-col items-center justify-center'>
+                                <h3 className='text-slate-950'>TypeScript</h3>
+                            </div>
                         </div>
-                        <div className='flex flex-col items-center justify-center'>
-                            <h3 className='text-slate-950'>TypeScript</h3>
-                        </div>
-                    </div>
+                    </Link>
                 </div>
                 <div className='p-6 shadow-xl rounded-xl bg-white hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <Link href={'https://www.typescriptlang.org'}><div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src={nextjs} alt="/" width={64} height={64} />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3 className='text-slate-950'>Next.JS</h3>
                         </div>
-                    </div>
+                    </div></Link>
+
                 </div>
                 <div className='p-6 shadow-xl rounded-xl bg-white hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <Link href={'https://github.com'}><div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src={github} alt="/" width={64} height={64} />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3 className='text-slate-950'>github</h3>
                         </div>
-                    </div>
+                    </div></Link>
                 </div>
                 <div className='p-6 shadow-xl rounded-xl bg-white hover:scale-105 ease-in duration-300'>
-                    <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+                    <Link href={'https://www.w3schools.com/js/'}><div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src={javascript} alt="/" width={64} height={64} />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
                             <h3 className='text-slate-950'>JavaScript</h3>
                         </div>
-                    </div>
+                    </div></Link>
+
                 </div>
             </div>
         </div>
