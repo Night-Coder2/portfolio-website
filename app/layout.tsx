1a2759bfdd57b64ccc92410e3539ef10af8f08e8
phoenixLegen <sudhircks@gmail.com>
2023-05-22 16:38:02 +0530

    favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,15 @@ import React from "react";
 import {Analytics} from "@vercel/analytics/react";
 import Link from "next/link";
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
           <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
           <title>Jeb&apos;s Portfolio</title>
           <meta charSet="UTF-8"/>
           <meta name="description" content="My Portfolio"/>
@@ -24,5 +28,3 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     </html>
   )
 }
-
-export default RootLayout;
