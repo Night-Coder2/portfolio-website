29fdbd1c9ffd48c246615eeaf55435bff5799bf7
phoenixLegen <sudhircks@gmail.com>
2023-05-22 19:55:04 +0530

    favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,27 @@ import '@/styles/globals.css'
 import Navbar from '@/components/Portfolio/Navbar'
 import React from "react";
 import {Analytics} from "@vercel/analytics/react";
-import Link from "next/link";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-          <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
-          <link rel="shortcut icon" href="/images/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-          <title>Jeb&apos;s Portfolio</title>
-          <meta charSet="UTF-8"/>
-          <meta name="description" content="My Portfolio"/>
-          <meta name="keywords" content="HTML, CSS, JavaScript"/>
-          <meta name="author" content="Jeb"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      </head>
-      <body>
-        <Navbar />
+    return (
+        <html lang="en">
+        <head>
+            <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
+            <link rel="shortcut icon" href="/favicon.ico"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <title>Jeb&apos;s Portfolio</title>
+            <meta charSet="UTF-8"/>
+            <meta name="description" content="My Portfolio"/>
+            <meta name="keywords" content="HTML, CSS, JavaScript"/>
+            <meta name="author" content="Jeb"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        </head>
+        <body>
+        <Navbar/>
         {children}
         <Analytics/>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
